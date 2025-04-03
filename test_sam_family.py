0c9cf500d72e0abccff96e1018f3a5d36d58f4ff
SiLang <33173982+SiLangWHL@users.noreply.github.com>
2025-04-04 02:01:18 +0800

    Add files via upload

--- test_sam_family.py
+++ test_sam_family.py
@@ -1,9 +1,10 @@
 from networks.sam.sam_base import SAM_Base
+from networks.sam.sam_ms import SAM_MS
 import torch
 
 # SAM_Base: frozen the pre-trained SAM and set the decoder learnable
-sam_base = SAM_Base(num_classes=2)
+sam_ms = SAM_MS(num_classes=2, dr=16.0)
 x = torch.randn(1, 1, 96, 96, 96) # batch, channel (default 1 for CT), Height, Width, Depth
-y = sam_base(x)
+y = sam_ms(x)
 print(y.size())
-print(sam_base)
+#print(sam_base)
